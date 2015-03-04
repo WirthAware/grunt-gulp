e734f8d243c64e5006aa42f8f298ffc2e7267bba
Stephan Wirth <stephan.gerbling@gmail.com>
2015-03-04 12:30:23 +0100

    added gulp tasks clean, copy, uglify, concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,55 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var pkg = require('./package');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+var size = require('gulp-size');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var clean = require('gulp-clean');
 
+// clean
+gulp.task('clean-all', function(){
+    return gulp.src([pkg.paths.dest.base])
+        .pipe(clean({force: true}))
+});
+
+
+// uglify
+// concat
+// copy
+gulp.task('bundlejs', ['jshint'], function () {
+    var bundlefile = pkg.name + ".min.js";
+    var opt = {newLine: ';'};
+
+    return gulp.src(pkg.paths.source.js)
+        .pipe(size({showFiles: true}))
+        .pipe(uglify())
+        .pipe(concat(bundlefile, opt))
+        .pipe(gulp.dest(pkg.paths.dest.js))
+        .pipe(size({showFiles: true}));
+});
+
+gulp.task('bundlecss', function () {
+    return gulp.src(pkg.paths.source.css)
+        .pipe(size({showFiles: true}))
+        // .pipe(plug.minifyCss({}))
+        .pipe(concat(pkg.name + ".min.css"))
+        .pipe(gulp.dest(pkg.paths.dest.css))
+        .pipe(size({showFiles: true}));
+});
+
+
+// jshint
+gulp.task('jshint', function () {
+    return gulp.src(pkg.paths.source.js)
+        .pipe(jshint('jshintrc.json'))
+//        .pipe(jshint.reporter('default'));
+        .pipe(jshint.reporter(stylish));
+});
+
+
+// watch with livereload
 gulp.task('connect', function() {
   connect.server({
       root: '',
@@ -9,17 +58,17 @@ gulp.task('connect', function() {
 });
 
 gulp.task('html', function () {
-  gulp.src(['index.html', './src/app/**/*.html'])
+  gulp.src(pkg.paths.source.html)
     .pipe(connect.reload());
 });
 
 gulp.task('js', function () {
-  gulp.src(['./src/app/**/*.js'])
+  gulp.src(pkg.paths.source.js)
     .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['index.html', './src/app/**/*.html', './src/app/**/*.js'], ['html', 'js']);
+  gulp.watch(pkg.paths.source.html.concat( pkg.paths.source.js ), ['jshint', 'html', 'js']);
 });
 
 gulp.task('default', ['connect', 'watch']);
