14708c37a56dd7dfbb356e64719799d66c843d9b
vslepakov <v.slepakov@gmail.com>
2015-03-18 13:04:00 +0100

    cleaned up

--- src/app/dashboard/dashboard.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict';
-    var controllerId = 'dashboard';
-    angular.module('app').controller(controllerId, [dashboard]);
-
-    function dashboard() {
-        var vm = this;
-    }
-})();
